bd66480d82660cab8d2913c8acff95900300c1b9
pointmarker <yagizalanli@gmail.com>
2025-08-22 19:22:26 +0300

    initial commit

---
+++ nodeDirScript.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# to making file management for creating this app file needs
+
+mkdir app
+cd app
+mkdir public environment models routes controllers services middleware config
+touch server.js
+cd public 
+mkdir pages js css
+cd ..
+cd environment
+touch environment.js
+cd ..
+cd models
+touch user.model.js
+cd ..
+cd routes
+touch index.js auth.route.js user.route.js
+cd ..
+cd controllers
+touch auth.controller.js user.controller.js
+cd ..
+cd services
+touch storageLocal.js storageS3.js
+cd ..
+mkdir uploads
